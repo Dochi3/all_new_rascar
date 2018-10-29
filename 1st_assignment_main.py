601c5cd8eec65051483d81eb580fc70fb1ecb526
sunjbs <sunjbs98@gmail.com>
2018-10-29 11:04:23 +0900

    2nd assignment ver.1.2.7

--- 1st_assignment_main.py
+++ 1st_assignment_main.py
@@ -72,9 +72,7 @@ class myCar(object):
                     count += 1
                 else:
                     count = 0
-                s = (weight * line) * (1 + rate * line[2])
-                print("s :", s)
-                print((s - before_s) * (-1 if s == 0 else 1))
+                s = numpy.dot(weight,line) * (1 + rate * line[2])
                 past_degree.append((s - before_s) * (-1 if s == 0 else 1))
                 print("done")
                 degree = multi_rate * past_degree
