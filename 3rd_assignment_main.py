ad108eb23f2c597915a6f63effbb840e2a9c4ca4
sunjbs <sunjbs98@gmail.com>
2018-11-14 11:21:46 +0900

    angle test source

--- 3rd_assignment_main.py
+++ 3rd_assignment_main.py
@@ -70,8 +70,8 @@ class myCar(object):
         return numpy.array(self.car.line_detector.read_digital())
     
     def turn(self, degree):
-        min_degree = -45
-        max_degree = 45
+        min_degree = -37
+        max_degree = 37
         degree = max(min_degree, min(max_degree, degree))
         self.car.steering.turn(90 + degree)
 
