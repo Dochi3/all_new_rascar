f10715bcb2c8eb66c434eaaf5b4126ceee5cce25
sunjbs <sunjbs98@gmail.com>
2018-11-15 16:04:25 +0900

    fix

--- 3rd_assignment_main.py
+++ 3rd_assignment_main.py
@@ -52,7 +52,7 @@ class myCar(object):
         self.car.steering.turn(90 + degree)
 
     def evading(self):
-        turn_left_angle = -35
+        turn_left_angle = -15
         turn_right_angle = 35
 
         self.move_front(40)
